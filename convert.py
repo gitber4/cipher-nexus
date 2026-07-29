3273427728896baccbc000c3896f332476d7b0f1
gitber4 <githuber4@gmail.com>
2026-07-30 01:23:49 +0500

    chore: добавлены файлы в .gitignore (config.ini, *.spec, convert.py)

--- convert.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# Открываем ваш PNG (желательно, чтобы он был квадратным, например 512x512)
-img = Image.open("ascii-art (4).png")
-
-# Сохраняем как ICO с набором стандартных размеров для Windows
-icon_sizes = [(16, 16), (32, 32), (48, 48), (256, 256)]
-img.save("my_icon.ico", sizes=icon_sizes)
-print("Конвертация успешна! Файл my_icon.ico создан.")
